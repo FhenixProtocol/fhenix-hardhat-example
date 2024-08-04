eedce44f8d62449977c4273e393866cec19cf65e
Cashmaney <itzik@fhenix.io>
2024-08-04 15:19:02 +0300

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,8 +13,8 @@ import {resolve} from "path";
 const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || "./.env";
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) });
 
-const TESTNET_CHAIN_ID = 42069;
-const TESTNET_RPC_URL = "https://api.testnet.fhenix.zone:7747";
+const TESTNET_CHAIN_ID = 8008135;
+const TESTNET_RPC_URL = "https://api.helium.fhenix.zone";
 
 const testnetConfig = {
     chainId: TESTNET_CHAIN_ID,
